45da3388c2f4f2e8b55378d63d0a935c009fb9e0
Eddy Garcia <kiribaty88@gmail.com>
2019-05-18 19:35:54 +0200

    added some layout to weather fragment

--- app/src/main/java/newton/travelassistant/WeatherFragment.java
+++ app/src/main/java/newton/travelassistant/WeatherFragment.java
@@ -6,11 +6,41 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class WeatherFragment extends Fragment {
+
+    TextView mCityTV;
+    EditText mCityET;
+    Button mButton;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_weather,container, false );
+        View view = inflater.inflate(R.layout.fragment_weather , container, false );
+
+
+        mCityTV = view.findViewById(R.id.cityTV);
+        mCityET = view.findViewById(R.id.cityET);
+        mButton = view.findViewById(R.id.myButton);
+
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mCityTV.getText().toString().equals("")){
+                    mCityTV.setText(mCityET.getText().toString());
+                }else{
+                    mCityTV.setText("");
+                }
+            }
+        });
+
+        return view;
     }
+
+
+
+
+
 }
